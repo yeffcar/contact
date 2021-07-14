104292e51f07fc1246ac265b558c72007a09f734
Yefferson <yeffcar@gmail.com>
2021-07-14 12:48:58 -0300

    FIX

--- app/Http/Controllers/NumController.php
+++ app/Http/Controllers/NumController.php
@@ -9,8 +9,9 @@ class NumController extends Controller
     
  //
  public function index()
- {
-     
+ {    
+
+    //CREANDO ALEATORIOS
     $valores = array();
     $max_num = 10;
 
@@ -19,8 +20,25 @@ class NumController extends Controller
         $num_aleatorio = rand(1,100);
         array_push($valores,$num_aleatorio);
     }
+    echo "10 Aleatorios";
+    echo "<br/>";
     var_dump ($valores);
+    echo "<br/>";
+    //VALORES IMPARES
 
+    
+
+    $impares = array_filter($valores, function ($var)
+    {
+        // Retorna siempre que el número entero sea impar
+        return($var & 1);
+    });
+
+    echo "Impares";
+    echo "<br/>";
+    var_dump($impares);
+    echo "<br/>";
+   
  }
 
 }
